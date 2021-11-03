aef6f6120e8d9cf5e1792f287dab9a828c8bd976
hamatan0716 <hama.uvuv0716@gmail.com>
2021-11-03 14:47:20 +0900

    アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :delivery_source
+  belongs_to :until_delivery
 end
