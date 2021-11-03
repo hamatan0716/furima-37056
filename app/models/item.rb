d2346bc320e2f3d9e1561fd544d283d389417a14
hamatan0716 <hama.uvuv0716@gmail.com>
2021-11-03 18:58:03 +0900

    エラーハンドリング

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
 
   validates :price, presence: true, format: { with: /\A[0-9]+\z/ }, numericality: {less_than_or_equal_to: 9999999, greater_than_or_equal_to: 300 }
   
-  with_options numericality: {orther_than: 1 } do
+  with_options numericality: {other_than: 1, message: "can't be blank"} do
     validates :category_id
     validates :status_id
     validates :delivery_fee_id
