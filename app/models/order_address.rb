eb3835ea6e538ea6c2cd00c7ec030133224374a9
hamatan0716 <hama.uvuv0716@gmail.com>
2021-11-09 00:33:46 +0900

    テストコード

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,10 +5,10 @@ class OrderAddress
   with_options presence: true do
     validates :item_id
     validates :user_id
-    validates :postcode, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :city
     validates :block
-    validates :phone_number, format: {with: /\A\d{10,11}\z/,}
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/,}
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_address do
+    postcode         { '123-1234' }
+    prefecture_id    { rand(2..48)}
+    city             { '福岡市' }
+    block            { '天神1−1−1' }
+    building         { 'sampleビル' }
+    phone_number     { '1234567890' }
+  end
+end
